5b2c80de7ed4f16be4426fbda9b069c99e06e82f
yel-m <coin6442@gmail.com>
2023-12-24 22:35:04 +0900

    ✨ useThree, useFrame, useRef 사용하기

--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,12 +1,29 @@
-import * as THREE from 'three';  // 내장 함수
+import * as THREE from "three";
+import { useThree, useFrame } from "@react-three/fiber"; // 내장 함수
+import { useRef } from 'react';
+
 export default function ThreeElement() {
-    return(
-        <>
-            <directionalLight position={[5,5,5]}/>
-            <mesh rotation={[0, THREE.MathUtils.degToRad(45), 0]}> 
-                <boxGeometry />
-                <meshStandardMaterial color="red" />
-            </mesh>
-        </>
-    )
+  const { size, gl, scene, camera } = useThree();
+  const boxRef = useRef<THREE.Mesh>(null);
+  useFrame((state, delta) => {
+    boxRef.current.rotation.x += delta;
+    boxRef.current.position.y -= 0.01;
+    boxRef.current.scale.z += 0.01;
+  })
+  return (
+    <>
+      <directionalLight position={[5, 5, 5]} />
+      <mesh
+        ref={boxRef}
+        rotation={[
+          THREE.MathUtils.degToRad(45),
+          THREE.MathUtils.degToRad(45),
+          0,
+        ]}
+        >
+        <boxGeometry />
+        <meshStandardMaterial color="red" />
+      </mesh>
+    </>
+  );
 }
