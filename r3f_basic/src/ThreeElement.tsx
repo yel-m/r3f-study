1306ab61b081b6bc8cc08bb26663fc7bdf12c571
yel-m <coin6442@gmail.com>
2024-01-01 18:25:29 +0900

    ✨ Scene (Mesh - Geometry + Meterial)

--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -5,12 +5,19 @@ import { useRef } from 'react';
 export default function ThreeElement() {
   const { size, gl, scene, camera } = useThree();
   const boxRef = useRef<THREE.Mesh>(null);
+
+  // Three.js 방식
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
   useFrame((state, delta) => {
     boxRef.current.rotation.x += delta;
     boxRef.current.position.y -= 0.01;
     boxRef.current.scale.z += 0.01;
   })
-  return (
+  return ( // R3F 방식
     <>
       <directionalLight position={[5, 5, 5]} />
       <mesh
