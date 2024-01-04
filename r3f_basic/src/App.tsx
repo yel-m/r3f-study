4b7fd7fc2538812c7470f87f34eee2b8d50084d2
yel-m <coin6442@gmail.com>
2024-01-04 18:49:34 +0900

    ✨ GUI Control (leva)

--- r3f_basic/src/App.tsx
+++ r3f_basic/src/App.tsx
@@ -2,8 +2,17 @@ import './App.css'
 import { Canvas } from '@react-three/fiber'
 import ThreeElement from './ThreeElement';
 import { OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
 
 function App() {
+
+  const color = useControls({
+    value: 'white',
+  })
+  
+  const grid = useControls({
+    segment: {value:10, min:2, max:100, step:1},
+  })
 
   return (
     <>
@@ -15,14 +24,10 @@ function App() {
           position:[3,3,0],
         }}
       >
-        <OrbitControls
-          minAzimuthAngle={-Math.PI / 4}
-          maxAzimuthAngle={Math.PI / 4}
-          minPolarAngle={Math.PI / 6}
-          maxPolarAngle={Math.PI - Math.PI / 6}
-        />
+        <color attach="background" args={[color.value]} />
+        <OrbitControls/>
         <axesHelper args={[6]}/>
-        <gridHelper args={[10, 10]}/>
+        <gridHelper args={[10, grid.segment]}/>
         <ThreeElement />
       </Canvas>
       R3F Basic
--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,30 +1,29 @@
 import * as THREE from "three";
 import { useThree, useFrame } from "@react-three/fiber"; // 내장 함수
 import { useRef } from 'react';
+import { useControls } from 'leva';
 
 export default function ThreeElement() {
   const { size, gl, scene, camera } = useThree();
   const boxRef = useRef<THREE.Mesh>(null);
 
-  // Three.js 방식
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  const box = useControls({
+    rotation: {value:0, min:-360, max:360, step:1},
+  })
 
   useFrame((state, delta) => {
-    boxRef.current.rotation.x += delta;
-    boxRef.current.position.y -= 0.01;
-    boxRef.current.scale.z += 0.01;
+    // boxRef.current.rotation.x += delta;
+    // boxRef.current.position.y -= 0.01;
+    // boxRef.current.scale.z += 0.01;
   })
-  return ( // R3F 방식
+  return ( 
     <>
       <directionalLight position={[5, 5, 5]} />
       <mesh
         ref={boxRef}
         rotation={[
           THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(45),
+          THREE.MathUtils.degToRad(box.rotation),
           0,
         ]}
         >
