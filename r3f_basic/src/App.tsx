121e9c76cb3b3ec8ab52d0c82dc171f1060e3194
yel-m <coin6442@gmail.com>
2024-01-04 18:51:20 +0900

    ✨ Transformation (위치, 크기, 회전)

--- r3f_basic/src/App.tsx
+++ r3f_basic/src/App.tsx
@@ -2,17 +2,8 @@ import './App.css'
 import { Canvas } from '@react-three/fiber'
 import ThreeElement from './ThreeElement';
 import { OrbitControls } from '@react-three/drei';
-import { useControls } from 'leva';
 
 function App() {
-
-  const color = useControls({
-    value: 'white',
-  })
-  
-  const grid = useControls({
-    segment: {value:10, min:2, max:100, step:1},
-  })
 
   return (
     <>
@@ -21,13 +12,13 @@ function App() {
           fov:75,
           near:1,
           far:100,
-          position:[3,3,0],
+          position:[5,5,5],
         }}
       >
-        <color attach="background" args={[color.value]} />
+        <color attach="background" args={['white']} />
         <OrbitControls/>
         <axesHelper args={[6]}/>
-        <gridHelper args={[10, grid.segment]}/>
+        <gridHelper args={[10, 10]}/>
         <ThreeElement />
       </Canvas>
       R3F Basic
