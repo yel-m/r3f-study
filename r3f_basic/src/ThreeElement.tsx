3c5280ed00726c6740aad033c21061ce46e35ad7
yel-m <coin6442@gmail.com>
2024-01-04 18:52:14 +0900

    ✨ Object3D (Scene, Group, Mesh)

--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,35 +1,37 @@
 import * as THREE from "three";
 import { useThree, useFrame } from "@react-three/fiber"; // 내장 함수
 import { useRef } from 'react';
-import { useControls } from 'leva';
 
 export default function ThreeElement() {
   const { size, gl, scene, camera } = useThree();
   const boxRef = useRef<THREE.Mesh>(null);
 
-  const box = useControls({
-    rotation: {value:0, min:-360, max:360, step:1},
-  })
-
   useFrame((state, delta) => {
     // boxRef.current.rotation.x += delta;
-    // boxRef.current.position.y -= 0.01;
+    // boxRef.current.position.y += 0.01;
     // boxRef.current.scale.z += 0.01;
+    scene.position.x += 0.01;
   })
   return ( 
     <>
       <directionalLight position={[5, 5, 5]} />
-      <mesh
-        ref={boxRef}
-        rotation={[
-          THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(box.rotation),
-          0,
-        ]}
+      <group
+        position={[5,0,0]}  
+      >
+        <mesh
+          ref={boxRef}
+          position={[0,0,0]}
+          scale={[1,1,1]}
+          rotation={[
+            THREE.MathUtils.degToRad(10),
+            THREE.MathUtils.degToRad(10),
+            THREE.MathUtils.degToRad(10),
+          ]}
         >
-        <boxGeometry />
-        <meshStandardMaterial color="red" />
-      </mesh>
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+      </group>
     </>
   );
 }
