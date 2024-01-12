ad3b862e82f0eedf037a26e14dfb0692f329e66a
yel-m <coin6442@gmail.com>
2024-01-12 18:50:14 +0900

    ✨ boxGeometry 옵션 (leva)

--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,12 +1,20 @@
 import * as THREE from "three";
 import { useThree, useFrame } from "@react-three/fiber"; // 내장 함수
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect } from "react";
+import { useControls } from "leva";
 
 export default function ThreeElement() {
   const { size, gl, scene, camera } = useThree();
   const boxRef = useRef<THREE.Mesh>(null);
   const boxCopyRef = useRef<THREE.Mesh>(null);
-
+  const boxControl = useControls({
+    width: { value: 1, min: 0.1, max: 10, step: 0.1 },
+    height: { value: 1, min: 0.1, max: 10, step: 0.1 },
+    depth: { value: 1, min: 0.1, max: 10, step: 0.1 },
+    widthSeg: { value: 1, min: 1, max: 10, step: 1 },
+    heightSeg: { value: 1, min: 1, max: 10, step: 1 },
+    depthSeg: { value: 1, min: 1, max: 10, step: 1 },
+  });
 
   useFrame((state, delta) => {
     // boxRef.current.rotation.x += delta;
@@ -15,26 +23,30 @@ export default function ThreeElement() {
     // scene.position.x += 0.01;
     // scene.rotation.x += 0.01;
     // groupRef.current.rotation.x += delta;
-  })
+  });
 
   useEffect(() => {
     boxCopyRef.current.geometry = boxRef.current.geometry;
-  }, [])
+  }, [boxControl]);
 
-  return ( 
+  return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <mesh
-        ref={boxRef}
-        position={[0,0,0]}
-      >
-        <boxGeometry />
-        <meshStandardMaterial wireframe/>
+      <mesh ref={boxRef} position={[0, 0, 0]}>
+        <boxGeometry
+          args={[
+            boxControl.width,
+            boxControl.height,
+            boxControl.depth,
+            boxControl.widthSeg,
+            boxControl.heightSeg,
+            boxControl.depthSeg,
+          ]}
+        />
+        <meshStandardMaterial wireframe />
       </mesh>
-      <mesh
-        ref={boxCopyRef}
-      >
-        <meshStandardMaterial color="red"/>
+      <mesh ref={boxCopyRef}>
+        <meshStandardMaterial color="red" />
       </mesh>
     </>
   );
