0825d76da3764d1f5637d595d52f9e156e5afcb2
yel-m <coin6442@gmail.com>
2024-01-14 19:00:28 +0900

    ✨ wireframe, basic, lambert material

--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,52 +1,46 @@
-import * as THREE from "three";
-import { useThree, useFrame } from "@react-three/fiber"; // 내장 함수
-import { useRef, useEffect } from "react";
-import { useControls } from "leva";
+import * as THREE from 'three';
+import { useFrame } from "@react-three/fiber"; // 내장 함수
+import { useEffect } from "react";
 
 export default function ThreeElement() {
-  const { size, gl, scene, camera } = useThree();
-  const boxRef = useRef<THREE.Mesh>(null);
-  const boxCopyRef = useRef<THREE.Mesh>(null);
-  const boxControl = useControls({
-    width: { value: 1, min: 0.1, max: 10, step: 0.1 },
-    height: { value: 1, min: 0.1, max: 10, step: 0.1 },
-    depth: { value: 1, min: 0.1, max: 10, step: 0.1 },
-    widthSeg: { value: 1, min: 1, max: 10, step: 1 },
-    heightSeg: { value: 1, min: 1, max: 10, step: 1 },
-    depthSeg: { value: 1, min: 1, max: 10, step: 1 },
-  });
+  useFrame((state, delta) => {});
 
-  useFrame((state, delta) => {
-    // boxRef.current.rotation.x += delta;
-    // boxRef.current.position.y += 0.01;
-    // boxRef.current.scale.z += 0.01;
-    // scene.position.x += 0.01;
-    // scene.rotation.x += 0.01;
-    // groupRef.current.rotation.x += delta;
-  });
-
-  useEffect(() => {
-    boxCopyRef.current.geometry = boxRef.current.geometry;
-  }, [boxControl]);
+  useEffect(() => {}, []);
 
   return (
     <>
-      <directionalLight position={[5, 5, 5]} />
-      <mesh ref={boxRef} position={[0, 0, 0]}>
-        <boxGeometry
-          args={[
-            boxControl.width,
-            boxControl.height,
-            boxControl.depth,
-            boxControl.widthSeg,
-            boxControl.heightSeg,
-            boxControl.depthSeg,
-          ]}
-        />
-        <meshStandardMaterial wireframe />
+      <directionalLight position={[5, 5, 5]}/>
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry />
+        <meshBasicMaterial color="green" />
       </mesh>
-      <mesh ref={boxCopyRef}>
-        <meshStandardMaterial color="red" />
+      <mesh position={[2, 0, 0]}>
+        <boxGeometry />
+        <meshBasicMaterial
+          color="red"
+          visible={true}
+          transparent={false}
+          opacity={1}
+          side={THREE.FrontSide}
+          alphaTest={1}
+          depthTest={true} 
+          depthWrite={true}
+        />
+      </mesh>
+      <mesh position={[4, 0, 0]}>
+        <boxGeometry />
+        <meshLambertMaterial
+          color="red"
+          visible={true}
+          transparent={false}
+          opacity={1}
+          side={THREE.FrontSide}
+          alphaTest={1}
+          depthTest={true} 
+          depthWrite={true}
+          fog={true}
+          emissive={'black'}
+        />
       </mesh>
     </>
   );
