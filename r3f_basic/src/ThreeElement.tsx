a4f325ab22d693da7ea468537424708abba1a5f1
yel-m <coin6442@gmail.com>
2024-01-14 19:20:32 +0900

    ♻️ group으로 묶고 자식 요소 공유

--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,47 +1,61 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import { useFrame } from "@react-three/fiber"; // 내장 함수
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function ThreeElement() {
+  const meshRef = useRef<THREE.Mesh>(null);
+  const groupRef = useRef<THREE.Group>(null);
+  // const meshCopyRef1 = useRef<THREE.Mesh>(null);
+  // const meshCopyRef2 = useRef<THREE.Mesh>(null);
+
   useFrame((state, delta) => {});
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // group의 children의 geometry를 meshRef의 geometry로 바꿔주는 작업
+    for (let i = 0; i < groupRef.current!.children.length; i++) {
+      const mesh = groupRef.current!.children[i] as THREE.Mesh;
+      mesh.geometry = meshRef.current!.geometry;
+      mesh.position.x = i * 2;
+    }
+    // meshCopyRef1.current!.geometry = meshRef.current!.geometry;
+    // meshCopyRef2.current!.geometry = meshRef.current!.geometry;
+  }, []);
 
   return (
     <>
-      <directionalLight position={[5, 5, 5]}/>
-      <mesh position={[0, 0, 0]}>
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <mesh ref={meshRef}>
         <boxGeometry />
-        <meshBasicMaterial color="green" />
+        <meshBasicMaterial visible={false} color="green" />
       </mesh>
-      <mesh position={[2, 0, 0]}>
-        <boxGeometry />
-        <meshBasicMaterial
-          color="red"
-          visible={true}
-          transparent={false}
-          opacity={1}
-          side={THREE.FrontSide}
-          alphaTest={1}
-          depthTest={true} 
-          depthWrite={true}
-        />
-      </mesh>
-      <mesh position={[4, 0, 0]}>
-        <boxGeometry />
-        <meshLambertMaterial
-          color="red"
-          visible={true}
-          transparent={false}
-          opacity={1}
-          side={THREE.FrontSide}
-          alphaTest={1}
-          depthTest={true} 
-          depthWrite={true}
-          fog={true}
-          emissive={'black'}
-        />
-      </mesh>
+      <group ref={groupRef}>
+        <mesh>
+          <meshBasicMaterial
+            color="red"
+            visible={true}
+            transparent={false}
+            opacity={1}
+            side={THREE.FrontSide}
+            alphaTest={1}
+            depthTest={true}
+            depthWrite={true}
+          />
+        </mesh>
+        <mesh>
+          <meshLambertMaterial
+            color="red"
+            visible={true}
+            transparent={false}
+            opacity={1}
+            side={THREE.FrontSide}
+            alphaTest={1}
+            depthTest={true}
+            depthWrite={true}
+            fog={true}
+            emissive={"black"}
+          />
+        </mesh>
+      </group>
     </>
   );
 }
