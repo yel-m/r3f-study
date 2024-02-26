7794404917b84a0044ee59d6973179f6391ab021
yel-m <coin6442@gmail.com>
2024-02-26 22:15:36 +0900

    ✨ 오브젝트 클릭 이벤트 전달 막기

--- r3f_basic/src/InteractionTest.tsx
+++ r3f_basic/src/InteractionTest.tsx
@@ -4,20 +4,35 @@ export default function InteractionTest() {
 
     function clickFunc(e:any) {
         console.log("clickFunc e : ", e);
-        e.object.material.color = new THREE.Color('green');
+        e.object.material.color = new THREE.Color('green'); 
+        e.stopPropagation();  
     }
-    
+
     return(
         <>
             <ambientLight />
             <directionalLight intensity={5} />
             <mesh
                 onClick={(e) => clickFunc(e)}
+                position={[-2,0,0]}
             >
                 <boxGeometry />
                 <meshStandardMaterial />
             </mesh>
-
+            <mesh
+                onClick={(e) => clickFunc(e)}
+                position={[0,0,0]}
+            >
+                <boxGeometry />
+                <meshStandardMaterial />
+            </mesh>
+            <mesh
+                onClick={(e) => clickFunc(e)}
+                position={[2,0,0]}
+            >
+                <boxGeometry />
+                <meshStandardMaterial />
+            </mesh>
         </>
     )
 }
