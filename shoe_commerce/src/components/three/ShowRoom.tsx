64ffacff805c7bfa1684ad18eabd0f23e8d7d033
yel-m <coin6442@gmail.com>
2024-02-27 03:50:45 +0900

    ♻️ 모듈 해석 개선

--- shoe_commerce/src/components/three/ShowRoom.tsx
+++ shoe_commerce/src/components/three/ShowRoom.tsx
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { useLoader } from '@react-three/fiber'
-import { useThree } from '@react-three/fiber'
+import { useLoader , useThree } from '@react-three/fiber'
 
 export default function ShowRoom() {
 
