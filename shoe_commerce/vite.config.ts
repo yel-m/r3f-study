30872f8093abf603209f4a890920a52f0be8cdc4
yel-m <coin6442@gmail.com>
2024-02-27 01:15:44 +0900

    🐛 쉼표 추가

--- shoe_commerce/vite.config.ts
+++ shoe_commerce/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       {
         find: "@src",
         replacement: path.resolve(__dirname, "src")
-      }
+      },
       {
         find: "@components",
         replacement: path.resolve(__dirname, "src/components")
