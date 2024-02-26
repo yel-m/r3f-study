f98960f3a8a5415c906f9fe55a066828eaa308cd
yel-m <coin6442@gmail.com>
2024-02-27 02:40:32 +0900

    ✨ GLB 파일로 3D 모델 불러오기 & 색상 변경

--- shoe_commerce/src/components/three/ShowRoom.tsx
+++ shoe_commerce/src/components/three/ShowRoom.tsx
@@ -1,17 +1,45 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useLoader } from '@react-three/fiber'
+import { useThree } from '@react-three/fiber'
 
 export default function ShowRoom() {
 
+    const { raycaster } = useThree();
+
     const obj = useLoader(OBJLoader, './models/custom.obj')
     const fbx = useLoader(FBXLoader, './models/custom.fbx')
+    const gltf = useLoader(GLTFLoader, './models/custom.glb')
+    
+    console.log("gltf : ", gltf);
 
+    const shoesClick = () => {
+
+        console.log("shoesClick");
+        const intersects = raycaster.intersectObjects(gltf.scene.children, true);
+        console.log("intersects: ", intersects);
+
+        if (intersects.length > 0) {
+            const firstObj = intersects[0].object as THREE.Mesh;
+            const firstMat = firstObj.material as THREE.MeshStandardMaterial;
+            const cloneMat = firstMat.clone();
+
+            firstObj.material = cloneMat;
+            const mat = firstObj.material as THREE.MeshStandardMaterial;
+            mat.color = new THREE.Color('red');
+
+        }
+    }
     return (
         <>
             {/* <primitive object={obj} /> */}
-            <primitive object={fbx} />
+            {/* <primitive object={fbx} /> */}
+            <primitive
+                object={gltf.scene}
+                onClick={shoesClick}
+            />
             {/* <mesh rotation={
                 [
                     THREE.MathUtils.degToRad(45),
