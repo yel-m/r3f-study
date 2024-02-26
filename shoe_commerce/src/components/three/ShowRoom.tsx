dd26e434527025b9733ed451f9c3c34c678a4b36
yel-m <coin6442@gmail.com>
2024-02-27 01:25:21 +0900

    ✨ FBX 파일 불러오기

--- shoe_commerce/src/components/three/ShowRoom.tsx
+++ shoe_commerce/src/components/three/ShowRoom.tsx
@@ -1,14 +1,17 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import { useLoader } from '@react-three/fiber'
 
 export default function ShowRoom() {
 
     const obj = useLoader(OBJLoader, './models/custom.obj')
+    const fbx = useLoader(FBXLoader, './models/custom.fbx')
 
     return (
         <>
-            <primitive object={obj} />
+            {/* <primitive object={obj} /> */}
+            <primitive object={fbx} />
             {/* <mesh rotation={
                 [
                     THREE.MathUtils.degToRad(45),
